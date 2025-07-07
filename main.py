bc93c3c3364455d2dad16af4b56ca494a98707e1
bbwinner <bbcam7392@gmail.com>
2025-07-07 13:11:53 +0800

    fix/input the url

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bbc_scraper import get_bbc_article_text
 from pdf_writer import save_pdf
 
-url = "https://www.bbc.com/news/articles/c07dmx38kyeo"
-# url = input("Please input the url: ")
+# url = "https://www.bbc.com/news/articles/c07dmx38kyeo"
+url = input("Please input the url: ")
 title, text = get_bbc_article_text(url)
 
 # print("TITLE:", title)
